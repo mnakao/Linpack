a368d77fd8630d440247d4662b5059e50bbcc69e
Masahiro Nakao <masahiro.nakao@riken.jp>
2015-07-17 13:57:22 +0900

    Add DTRSV with blocking in dsolve

--- lu_solve.c
+++ lu_solve.c
@@ -1,10 +1,9 @@
 #include "common.h"
 
-#define _BLAS
+//#define _BLAS
+#define _BLAS_BLOCK
 //#define _OUTER_PRODUCT
-//#define _OUTER__PRODUCT_BLOCKING
 //#define _INNER_PRODUCT
-//#define _INNER_PRODUCT_BLOCKING
 void lu_solve(const int N, const int NB, const int LD, double (*A)[LD], double *b)
 {
   timer_start(DSOLVE);
@@ -12,21 +11,42 @@ void lu_solve(const int N, const int NB, const int LD, double (*A)[LD], double *
 #if defined _BLAS
   cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
 	      N, &A[0][0], LD, b, 1);
+#elif defined _BLAS_BLOCK
+  double x[NB];
+
+  int rest = N % NB;
+  rest = (rest == 0)? NB : rest;
+  int k = N - rest;
+
+  cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
+              rest, &A[k][k], LD, &b[k], 1);
+  cblas_dcopy(rest, &b[k], 1, x, 1);
+
+  cblas_dgemv(CblasColMajor, CblasNoTrans, NB, rest, -1.0,
+	      &A[k][k-NB], LD, x, 1, 1.0, &b[k-NB], 1);
+  cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
+	      NB, &A[k-NB][k-NB], LD, &b[k-NB], 1);
+  cblas_dcopy(NB, &b[k-NB], 1, x, 1);
+
+  cblas_dgemv(CblasColMajor, CblasNoTrans, k-NB, rest, -1.0,
+	      &A[k][0], LD, &b[k], 1, 1.0, &b[0], 1);
+
+  for(k=N-rest-NB;k>=NB;k-=NB){
+    cblas_dgemv(CblasColMajor, CblasNoTrans, NB, NB, -1.0,
+                &A[k][k-NB], LD, x, 1, 1.0, &b[k-NB], 1);
+    cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
+		NB, &A[k-NB][k-NB], LD, &b[k-NB], 1);
+    cblas_dcopy(NB, &b[k-NB], 1, x, 1);
+    cblas_dgemv(CblasColMajor, CblasNoTrans, k-NB, NB, -1.0,
+		&A[k][0], LD, &b[k], 1, 1.0, &b[0], 1);
+  }
+
 #elif defined _OUTER_PRODUCT
   for(int i=N-1;i>=0;i--){
     double tmp = b[i] /= A[i][i];
     for(int k=0;k<i;k++)
       b[k] -= tmp * A[i][k];
 
-  }
-#elif defined _OUTER__PRODUCT_BLOCKING
-  for(int i=N-1;i>=0;i-=NB){
-    int end = Mmax(i-NB+1, 0);
-    for(int j=i;j>=end;j--){
-      double tmp = b[j] /= A[j][j];
-      for(int k=0;k<j;k++)
-	b[k] -= tmp * A[j][k];
-    }
   }
 #elif defined _INNER_PRODUCT
   b[N-1] /= A[N-1][N-1];
@@ -36,18 +56,6 @@ void lu_solve(const int N, const int NB, const int LD, double (*A)[LD], double *
       tmp -= A[j][k] * b[j];
 
     b[k] = tmp/A[k][k];
-  }
-#elif defined _INNER_PRODUCT_BLOCKING
-  b[N-1] /= A[N-1][N-1];
-  for(int k=N-2;k>=0;k-=NB){
-    int end = Mmax(k-NB+1, 0);
-    for(int i=k;i>=end;i--){
-      double tmp = b[i];
-      for(int j=i+1;j<N;j++)
-        tmp -= A[j][i] * b[j];
-
-      b[i] = tmp/A[i][i];
-    }
   }
 #endif
 
