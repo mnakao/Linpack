88af884fdcd6f4cadc65e41ca854e11da55f1980
Masahiro Nakao <masahiro.nakao@riken.jp>
2015-07-18 21:47:30 +0900

    Minor speed up

--- lu_solve.c
+++ lu_solve.c
@@ -11,6 +11,7 @@ void lu_solve(const int N, const int NB, const int LD, double (*A)[LD], double *
 #if defined _BLAS
   cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
 	      N, &A[0][0], LD, b, 1);
+  SOL();
 #elif defined _BLAS_BLOCK
   double x[NB];
 
@@ -19,27 +20,21 @@ void lu_solve(const int N, const int NB, const int LD, double (*A)[LD], double *
   int k = N - rest;
 
   cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
-              rest, &A[k][k], LD, &b[k], 1);
+	      rest, &A[k][k], LD, &b[k], 1);
   cblas_dcopy(rest, &b[k], 1, x, 1);
+  cblas_dgemv(CblasColMajor, CblasNoTrans, k, rest, -1.0,
+              &A[k][0], LD, x, 1, 1.0, b, 1);
 
-  cblas_dgemv(CblasColMajor, CblasNoTrans, NB, rest, -1.0,
-    	      &A[k][k-NB], LD, x, 1, 1.0, &b[k-NB], 1);
-  cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
-	      NB, &A[k-NB][k-NB], LD, &b[k-NB], 1);
-  cblas_dcopy(NB, &b[k-NB], 1, x, 1);
-  
-  cblas_dgemv(CblasColMajor, CblasNoTrans, k-NB, rest, -1.0,
-	      &A[k][0], LD, &b[k], 1, 1.0, &b[0], 1);
-
-  for(k=N-rest-NB;k>=NB;k-=NB){
-    cblas_dgemv(CblasColMajor, CblasNoTrans, NB, NB, -1.0,
-                &A[k][k-NB], LD, x, 1, 1.0, &b[k-NB], 1);
+  for(k=N-rest;k>NB;k-=NB){
     cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
-		NB, &A[k-NB][k-NB], LD, &b[k-NB], 1);
+                NB, &A[k-NB][k-NB], LD, &b[k-NB], 1);
     cblas_dcopy(NB, &b[k-NB], 1, x, 1);
     cblas_dgemv(CblasColMajor, CblasNoTrans, k-NB, NB, -1.0,
-		&A[k][0], LD, &b[k], 1, 1.0, &b[0], 1);
+                &A[k-NB][0], LD, x, 1, 1.0, b, 1);
   }
+
+  cblas_dtrsv(CblasColMajor, CblasUpper, CblasNoTrans, CblasNonUnit,
+	      NB, &A[0][0], LD, b, 1);
 
 #elif defined _OUTER_PRODUCT
   for(int i=N-1;i>=0;i--){
